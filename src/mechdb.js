ffbcf933278d54a3d9341e56349084577ad3a5fc
Kai Henningsen <kai.extern+git@gmail.com>
2018-11-25 16:29:29 +0100

    mechdb

---
+++ src/mechdb.js
@@ -0,0 +1,121 @@
+const util = require('util')
+
+const appId = 0x4d656368
+const userVer = 2
+
+// while initializing, use throw
+const sqlite3 = require('sqlite3').verbose()
+const db = new sqlite3.Database('.mechanic.db', err => {
+  if (err) {
+    throw err
+  }
+  db.get('pragma application_id', [], (err, row) => {
+    if (err) {
+      throw err
+    }
+    if (row.application_id === appId) {
+      db.get('pragma user_version', [], (err, row) => {
+        if (err) {
+          throw err
+        }
+        if (row.user_version === userVer) {
+          return db
+        } else {
+          rebuild()
+          return db
+        }
+      })
+    } else {
+      db.get('pragma schema_version', [], (err, row) => {
+        if (err) {
+          throw err
+        }
+        if (row.schema_version === 0) {
+          rebuild()
+          return db
+        } else {
+          throw Error('Found .mechanic.db but it is not a Mechanic DB')
+        }
+      })
+    }
+  })
+})
+
+function rebuild () {
+  // try to empty
+  db.all("select * from sqlite_master where type = 'table'", [], (err, rows) => {
+    if (err) {
+      throw err
+    }
+    db.serialize(() => {
+      rows.forEach(row => db.run(`drop table ${row.name}`))
+      // just in case
+      db.run(`pragma user_version(0)`)
+      // mark as mine
+      db.run(`pragma application_id(${appId})`)
+
+      // dependencies.status is a JSON object for files, or the content for variables
+      // dependlist is a JSON object of an array of 2-element arrays (dependencies.rowid, dependencies.generation)
+      db.run('create table dependencies(name text primary key, generation integer, status text)')
+      db.run('create table targets(name text primary key, dependlist text)')
+
+      // now at this version
+      db.run(`pragma user_version(${userVer})`)
+    })
+  })
+}
+
+// after initializing, use promises
+exports.getTarget = util.promisify((name, callback) => {
+  db.get('select dependlist from targets where name = ?', [name], (err, row) => {
+    if (err) {
+      callback(err, null)
+    } else {
+      callback(null, JSON.parse(row.dependlist))
+    }
+  })
+})
+
+exports.setTarget = util.promisify((name, dependlist, callback) => {
+  dependlist = JSON.stringify(dependlist)
+  exports.getDependency(name).then(old => {
+    if (old) {
+      if (old.dependlist === dependlist) {
+        callback(null, null)
+      } else {
+        db.run('update targets set dependlist = ? where name = ?', [dependlist, name], err => callback(err, null))
+      }
+    } else {
+      db.run('insert into targets(name, dependlist) values(?, ?)', [name, dependlist], err => callback(err, null))
+    }
+  }).catch(err => callback(err, null))
+})
+
+exports.getDependency = util.promisify((name, callback) => {
+  db.get('select generation, status from dependencies where name = ?', [name], (err, row) => {
+    if (err) {
+      callback(err, null)
+    } else {
+      callback(null, { generation: row.generation, status: JSON.parse(row.status) })
+    }
+  })
+})
+
+exports.setDependency = util.promisify((name, status, callback) => {
+  status = JSON.stringify(status)
+  exports.getDependency(name).then(old => {
+    if (old) {
+      if (old.status === status) {
+        callback(null, null)
+      } else {
+        db.run('update dependencies set generation = generation + 1, status = ? where name = ?', [status, name], err => callback(err, null))
+      }
+    } else {
+      db.run('insert into dependencies(name, generation, status) values(?, 1, ?)', [name, status], err => callback(err, null))
+    }
+  }).catch(err => callback(err, null))
+})
+
+exports.close = util.promisify(callback => {
+  db.close(err => callback(err, null))
+})
