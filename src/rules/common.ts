4a59ac65fe8c5dcc093c3da80a14d70024b0dc35
Kai Henningsen <kai.extern+git@gmail.com>
2018-12-09 14:19:01 +0100

    Use licenser & pretty

--- src/rules/common.ts
+++ src/rules/common.ts
@@ -1,39 +1,30 @@
+/**
+ * Copyright (c) 2018 Kai Henningsen <kai.extern+mechanic@gmail.com>
+ *
+ * This software is released under the MIT License.
+ * https://opensource.org/licenses/MIT
+ */
 
-// MIT License
-//
-// Copyright (c) 2018 Kai Henningsen
-//
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
-//
-// The above copyright notice and this permission notice shall be included in
-// all copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-// SOFTWARE.
+// tslint:disable-next-line:interface-over-type-literal
+export type VarTree = {
+  [ns: string]:
+    | {
+        [vn: string]: string | undefined;
+      }
+    | undefined;
+};
 
-export type VarTree = Map<string, Map<string, string>>;
-
-export interface ITargetDetails extends RegExpMatchArray {
-    targets: string[];
-}
+export type ITargetDetails = string[];
 export interface ITargetMatcher {
-    match(full: string, parent: string, child: string): ITargetDetails | null;
+  match(full: string, parent: string, child: string): ITargetDetails | null;
+  generate(vars: VarTree): string;
+  toString(): string;
 }
-export type TargetSpec = string|ITargetMatcher|[string, string|ITargetMatcher];
+export type TargetSpec = string | ITargetMatcher | [string, string | ITargetMatcher];
 
 export interface IDependencyGenerator {
-    generate(details: ITargetDetails, vars: VarTree): string[];
+  generate(vars: VarTree): string[];
 }
-export type DependencySpec = string|IDependencyGenerator;
+export type DependencySpec = string | IDependencyGenerator;
 
 export type CallbackR = (vars: VarTree) => void;
--- src/rules/tmAnalyse.ts
+++ src/rules/tmAnalyse.ts
@@ -1,56 +1,62 @@
-// MIT License
-//
-// Copyright (c) 2018 Kai Henningsen
-//
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
-//
-// The above copyright notice and this permission notice shall be included in
-// all
-// copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-// SOFTWARE.
+/**
+ * Copyright (c) 2018 Kai Henningsen <kai.extern+mechanic@gmail.com>
+ *
+ * This software is released under the MIT License.
+ * https://opensource.org/licenses/MIT
+ */
 
-import nanomatch = require("nanomatch");
-import { ITargetDetails, ITargetMatcher, TargetSpec } from "./common";
+import { ITargetDetails, ITargetMatcher, TargetSpec, VarTree } from "./common";
 
 class StringMatcher implements ITargetMatcher {
+  private readonly fp: string[];
   private readonly rx: RegExp;
-  constructor(m: string) {
-    this.rx = nanomatch.makeRe(m, { capture: true });
+  constructor(private readonly m: string) {
+    this.fp = m.split("%");
+    this.rx = new RegExp(
+      this.fp
+        .map((v: string) => {
+          return [...v]
+            .map((c: string) => {
+              c.replace(/\W/, "\\$&");
+            })
+            .join("");
+        })
+        .join("(.*)"),
+    );
   }
   public match(_: string, __: string, child: string): ITargetDetails | null {
-    return Object.assign({ targets: [] }, this.rx.exec(child));
+    return this.rx.exec(child);
+  }
+  public generate(vars: VarTree): string {
+    const n: { [vn: string]: string | undefined } = vars[""] || {};
+    let r = "";
+    for (let i = 0; i < this.fp.length; i++) {
+      r += this.fp[i] + (n["$" + i] || "");
+    }
+    return r;
+  }
+  public toString(): string {
+    return this.m;
   }
 }
 
-export type TMAnalysed = Array<{ ns: string; matcher: ITargetMatcher }>;
+export type TMAnalysed = Array<{ pathvar: string; matcher: ITargetMatcher }>;
 
 export function tmAnalyse(targets: TargetSpec[]): TMAnalysed {
   const ret: TMAnalysed = [];
   targets.forEach((target) => {
     if (Array.isArray(target)) {
       const [n, m] = target;
-      ret.push({ ns: n, matcher: typeof m === "string" ? new StringMatcher(m) : m });
+      ret.push({ pathvar: n, matcher: typeof m === "string" ? new StringMatcher(m) : m });
     } else if (typeof target === "string") {
       const m = /^(\w+):(.*)$/.exec(target);
       if (m) {
-        ret.push({ ns: m[1], matcher: new StringMatcher(m[2]) });
+        ret.push({ pathvar: m[1], matcher: new StringMatcher(m[2]) });
       } else {
-        ret.push({ ns: "", matcher: new StringMatcher(target) });
+        ret.push({ pathvar: "", matcher: new StringMatcher(target) });
       }
     } else {
-      ret.push({ ns: "", matcher: target });
+      ret.push({ pathvar: "", matcher: target });
     }
   });
   return ret;
