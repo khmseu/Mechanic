84d5e9a3d2688dbd8643a993a3ce583129a329f0
Kai Henningsen <kai.extern+git@gmail.com>
2019-01-16 16:28:12 +0100

    Test

--- makechangelog.sh
+++ makechangelog.sh
@@ -4,5 +4,5 @@ echo '# ChangeLog'
 echo
 git log --decorate --date=iso | \
   sed -e '/[^ ]/s,^ ,-&,' -e 's,^\(commit.*[^ ]\) *(\(.*\))$,## \2\n\n\1,g' | \
-  sed -e '/^c/{N;N;s,\n, ,g}' -e 's,^commit *\(.*\) Author: *\(.*\) Date: *\(.*\),### \1 \3 \2,'
+  sed -e '/^c/{N;N;s,\n, ,g}' -e 's,^commit *\(.*\) Author: *\(.*\) Date: *\(.*\),### https://github.com/khmseu/Mechanic/commit/\1 \3 \2,'
 echo
