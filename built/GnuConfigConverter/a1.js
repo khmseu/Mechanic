01e9a35f6ea7d0596c961634e8d12b40b277c040
Kai Henningsen <kai.extern+git@gmail.com>
2019-04-07 00:48:38 +0200

    Cleanup

--- built/GnuConfigConverter/a1.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYTEuanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi8uLi9zcmMvR251Q29uZmlnQ29udmVydGVyL2ExLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiIifQ==
--- dout/GnuConfigConverter/a1.d.ts
+++
@@ -1,2 +0,0 @@
-export {};
-//# sourceMappingURL=a1.d.ts.map
